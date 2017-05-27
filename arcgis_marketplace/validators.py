890479a23cc785706e9c3db59b0993e05f591ae2
mongkok <dani.pyc+github@gmail.com>
2017-05-27 22:54:49 +0200

    Use pathlib to get extension Python 3.x

--- arcgis_marketplace/validators.py
+++ arcgis_marketplace/validators.py
@@ -1,19 +1,20 @@
-import os
 import zipfile
+
+from pathlib import Path
 
 from django.core import exceptions
 from django.utils.translation import ugettext_lazy as _
 
 
 def validate_file_extension(value):
-    extension = os.path.splitext(value.name)[1]
+    extension = Path(value.name).suffix
 
     if extension not in ('.zip',):
         raise exceptions.ValidationError(_('File extension not supported'))
 
 
 def validate_zip_compression(value):
-    extension = os.path.splitext(value.name)[1]
+    extension = Path(value.name).suffix
 
     if extension == '.zip' and\
             (not zipfile.is_zipfile(value) or
