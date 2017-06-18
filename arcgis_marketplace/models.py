9e2b86120975729bf63ee2c667543df319367c50
mongkok <domake.io@gmail.com>
2017-06-18 22:44:57 +0200

    Orders inherance and minor improvements

--- arcgis_marketplace/models.py
+++ arcgis_marketplace/models.py
@@ -5,7 +5,6 @@ from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.contrib.postgres import fields as pg_fields
 from django.core.files.base import ContentFile
-from django.core.validators import MinValueValidator
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
@@ -16,7 +15,7 @@ from core_flavor.shortcuts import camel_to_dashed
 from core_flavor.urls import reverse_host
 
 from model_utils import Choices
-from polymorphic.models import PolymorphicModel
+from orders_flavor import models as orders_models
 from sorl.thumbnail import ImageField
 
 from taggit import models as taggit_models
@@ -33,16 +32,14 @@ class Account(core_models.SoftDeletableModel,
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
-        verbose_name=_('user')
-    )
+        verbose_name=_('user'))
 
     avatar = ImageField(
         _('avatar'),
         blank=True,
         upload_to=core_models.UUIDUploadTo(
             arcgis_settings.ARCGIS_UPLOAD_THUMBNAILS_TO
-        )
-    )
+        ))
 
     expired = models.DateTimeField(null=True)
     data = pg_fields.JSONField()
@@ -51,7 +48,7 @@ class Account(core_models.SoftDeletableModel,
         ordering = ('-created',)
 
     def __str__(self):
-        return self.user.email
+        return str(self.user)
 
     def __dir__(self):
         return super().__dir__() + list(self.data.keys())
@@ -68,7 +65,7 @@ class Account(core_models.SoftDeletableModel,
         return reverse_host(
             'arcgis-marketplace-api:v1:account-detail',
             args=(self.id.hex,),
-            **arcgis_settings.ARCGIS_REVERSE_EXTRA_KWARGS
+            **arcgis_settings.FLAVOR_REVERSE_EXTRA_KWARGS
         )
 
     @property
@@ -88,13 +85,13 @@ class Account(core_models.SoftDeletableModel,
 
     def refresh_expired_token(self):
         assert hasattr(settings, 'SOCIAL_AUTH_ARCGIS_KEY'), (
-            'Settings var SOCIAL_AUTH_ARCGIS_KEY missing'
+            'Missing "SOCIAL_AUTH_ARCGIS_KEY" settings var'
         )
 
         result = self.client().refresh_token(
             client_id=settings.SOCIAL_AUTH_ARCGIS_KEY,
-            refresh_token=self.refresh_token
-        )
+            refresh_token=self.refresh_token)
+
         self.data['access_token'] = result['access_token']
         self.set_expiration(result['expires_in'])
         self.save()
@@ -130,8 +127,8 @@ class Account(core_models.SoftDeletableModel,
         return self.self()['subscriptionInfo']['type']
 
     def save_thumbnail(self, thumbnail):
-        if thumbnail and \
-                (not self.avatar.name or thumbnail != self.thumbnail):
+        if thumbnail and (
+                not self.avatar.name or thumbnail != self.thumbnail):
 
             content = ContentFile(
                 self.api.user_thumbnail(
@@ -149,27 +146,11 @@ class GenericUUIDTaggedItem(taggit_models.CommonGenericTaggedItemBase,
     object_id = models.UUIDField(editable=False)
 
 
-class Item(PolymorphicModel,
-           core_models.SoftDeletableModel,
-           core_models.TimeStampedUUIDModel):
-
+class AbstractItem(core_models.SoftDeletableModel, orders_models.Item):
     owner = models.ForeignKey(
         'Account',
         on_delete=models.CASCADE,
-        verbose_name=_('owner')
-    )
-
-    title = models.CharField(_('title'), max_length=255)
-    description = models.TextField(_('description'), blank=True)
-    price = models.FloatField(_('price'), validators=[MinValueValidator(0)])
-
-    image = ImageField(
-        _('image'),
-        blank=True,
-        upload_to=core_models.UUIDUploadTo(
-            arcgis_settings.ARCGIS_UPLOAD_THUMBNAILS_TO
-        )
-    )
+        verbose_name=_('owner'))
 
     youtube_url = models.CharField(
         blank=True,
@@ -179,27 +160,19 @@ class Item(PolymorphicModel,
                 regex=r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',
                 message=_('Invalid youtube url')
             )
-        ]
-    )
+        ])
 
     objects = managers.ItemManager()
     tags = TaggableManager(blank=True, through=GenericUUIDTaggedItem)
 
     class Meta:
-        ordering = ('-created',)
+        abstract = True
 
     def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse_host(
-            'arcgis-marketplace-api:v1:product-detail',
-            args=(self.id.hex,),
-            **arcgis_settings.ARCGIS_REVERSE_EXTRA_KWARGS
-        )
+        return self.name
 
 
-class PurposeItem(Item):
+class AbstractPurposeItem(AbstractItem):
     PURPOSES = Choices(
         ('ready_to_use', _('Ready to use')),
         ('configurable', _('Configurable')),
@@ -214,7 +187,7 @@ class PurposeItem(Item):
         abstract = True
 
 
-class WebMapingApp(PurposeItem):
+class WebMapingApp(AbstractPurposeItem):
     APIS = Choices(
         ('javascript', _('Javascript')),
         ('flex', _('Flex')),
@@ -228,7 +201,6 @@ class WebMapingApp(PurposeItem):
     file = fields.CompressField(
         upload_to=core_models.UUIDUploadTo(
             arcgis_settings.ARCGIS_UPLOAD_ITEM_TO
-        )
-    )
+        ))
 
     preview = fields.SymlinkField(_('preview'), blank=True, source='file')
