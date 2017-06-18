5323f180e883b921920df691f0fd0014197ad38d
mongkok <domake.io@gmail.com>
2017-06-18 22:44:09 +0200

    Rename FLAVOR_REVERSE_EXTRA_KWARGS settings var

--- arcgis_marketplace/settings.py
+++ arcgis_marketplace/settings.py
@@ -4,17 +4,14 @@ from django.conf import settings
 ARCGIS_UPLOAD_ITEM_TO = getattr(
     settings,
     'ARCGIS_UPLOAD_ITEM_TO',
-    'arcgis/items/%Y/%m/'
-)
+    'arcgis/items/%Y/%m/')
 
 ARCGIS_UPLOAD_THUMBNAILS_TO = getattr(
     settings,
     'ARCGIS_UPLOAD_THUMBNAILS_TO',
-    'arcgis/thumbnails/%Y/%m/'
-)
+    'arcgis/thumbnails/%Y/%m/')
 
-ARCGIS_REVERSE_EXTRA_KWARGS = getattr(
+FLAVOR_REVERSE_EXTRA_KWARGS = getattr(
     settings,
-    'ARCGIS_REVERSE_EXTRA_KWARGS',
-    {}
-)
+    'FLAVOR_REVERSE_EXTRA_KWARGS',
+    {})
