25ac7a40d93861642e81036b016db4e2a8a80b17
mongkok <domake.io@gmail.com>
2017-06-18 22:45:06 +0200

    i18n

--- arcgis_marketplace/apps.py
+++ arcgis_marketplace/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ArcgisMarketplaceAppConfig(AppConfig):
     name = 'arcgis_marketplace'
-    verbose_name = 'Arcgis marketplace'
+    verbose_name = _('Arcgis marketplace')
