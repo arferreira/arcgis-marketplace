0e0eaa6edbb8d2a2a5edf3a771f4d0ad2225bb4a
mongkok <domake.io@gmail.com>
2017-06-18 22:43:44 +0200

    Using orders factories

--- arcgis_marketplace/factories.py
+++ arcgis_marketplace/factories.py
@@ -1,18 +1,15 @@
 import factory
 import factory.fuzzy
 
-from django.conf import settings
 from django.utils.timezone import utc
+
+from core_flavor import factories as core_factories
+from orders_flavor import factories as orders_factories
 
 from . import models
 
 
-class UserFactory(factory.django.DjangoModelFactory):
-    email = factory.Faker('email')
-
-    class Meta:
-        model = settings.AUTH_USER_MODEL
-        django_get_or_create = ('email',)
+class UserFactory(core_factories.UserFactory):
 
     @factory.post_generation
     def social_auth(self, create, extracted, **kwargs):
@@ -33,15 +30,13 @@ class AccountFactory(factory.django.DjangoModelFactory):
     data = dict(
         access_token='test',
         refresh_token='test',
-        username='test'
-    )
+        username='test')
 
     expired = factory.Faker(
         'date_time_between',
         start_date='+1d',
         end_date='+2d',
-        tzinfo=utc
-    )
+        tzinfo=utc)
 
     class Meta:
         model = 'arcgis_marketplace.Account'
@@ -51,23 +46,19 @@ class ExpiredAccountFactory(AccountFactory):
     expired = factory.Faker(
         'date_time_between',
         end_date='-1d',
-        tzinfo=utc
-    )
+        tzinfo=utc)
 
 
-class ItemFactory(factory.django.DjangoModelFactory):
+class AbstractItemFactory(orders_factories.ItemFactory):
     owner = factory.SubFactory(AccountFactory)
-    title = factory.fuzzy.FuzzyText(length=255)
-    price = factory.fuzzy.FuzzyFloat(1, 100)
 
     class Meta:
-        model = 'arcgis_marketplace.Item'
+        abstract = True
 
 
-class PurposeItemFactory(ItemFactory):
+class PurposeItemFactory(AbstractItemFactory):
     purpose = factory.fuzzy.FuzzyChoice(
-        choices=models.WebMapingApp.PURPOSES._db_values
-    )
+        choices=models.WebMapingApp.PURPOSES._db_values)
 
     class Meta:
         abstract = True
