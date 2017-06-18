6b5e0954df17bfd44011b6b8c8ec5af04854ae5a
mongkok <domake.io@gmail.com>
2017-06-18 22:40:33 +0200

    Remove Paypal payments

--- arcgis_marketplace/urls.py
+++ arcgis_marketplace/urls.py
@@ -1,17 +1,10 @@
-from django.conf.urls import include
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
-
-from . import views
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(
         template_name='arcgis_marketplace/index.html')),
-
-    url(r'^items/(?P<pk>[0-9a-f]{32})/paypal-form$',
-        views.ItemPaypalFormView.as_view(),
-        name='item-paypal-form'),
 
     url(r'^api/', include(
         'arcgis_marketplace.api.urls',
