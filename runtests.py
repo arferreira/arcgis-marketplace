04d4834f44154cd38b963e13d4a225410f8b416d
mongkok <domake.io@gmail.com>
2017-06-25 23:13:59 +0200

    Add django.contrib.sites to INSTALLED_APPS

--- runtests.py
+++ runtests.py
@@ -15,6 +15,7 @@ DEFAULT_SETTINGS = dict(
     INSTALLED_APPS=(
         'django.contrib.auth',
         'django.contrib.contenttypes',
+        'django.contrib.sites',
         'django_filters',
         'oauth2_provider',
         'paypal.standard.ipn',
