bbce98c5fc84cf3a7054b8006650df5875a71f8c
mongkok <domake.io@gmail.com>
2017-06-25 22:59:50 +0200

    Order succeeded logger handler

--- arcgis_marketplace/apps.py
+++ arcgis_marketplace/apps.py
@@ -5,3 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class ArcgisMarketplaceAppConfig(AppConfig):
     name = 'arcgis_marketplace'
     verbose_name = _('Arcgis marketplace')
+
+    def ready(self):
+        import arcgis_marketplace.signals.loggers  # NOQA
