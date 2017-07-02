840b3fefb672a7fec24161b90d8b1a7faf6b911d
Dani <domake.io@gmail.com>
2017-07-02 23:41:23 +0200

    Use id’s on task arguments

--- arcgis_marketplace/tasks.py
+++ arcgis_marketplace/tasks.py
@@ -4,14 +4,16 @@ from . import models
 
 
 @current_app.task()
-def add_item_to_account(account, item, order=None, group=None):
+def add_item_to_account(account_id, item_id, group=None):
+    account = models.objects.get(id=account_id)
+    item = models.objects.get(id=item_id)
+
     if group is None:
         group = account.get_group_for_apps()
 
     item_in_account = models.ItemInAccount.objects.create_item(
         account=account,
         item=item,
-        order=order,
         arcgis_group=group)
 
     account.share_items(
