0f43543c54e23b1c416641c3ecc8d61c08bbb1a8
mongkok <domake.io@gmail.com>
2017-07-09 15:22:55 +0200

    Fix WebMapingAppTxtFactory!

--- tests/factories.py
+++ tests/factories.py
@@ -1,9 +1,12 @@
+import factory
+
 from arcgis_marketplace import factories
 
 
 class WebMapingAppTxtFactory(factories.WebMapingAppFactory):
-    file__filename = 'test.txt'
-    file__from_path = 'tests/test.txt'
+    file = factory.django.FileField(
+        filename='test.txt',
+        from_path='tests/test.txt')
 
 
 class WebMapingAppZipFactory(factories.WebMapingAppFactory):
