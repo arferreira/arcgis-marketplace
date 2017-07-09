5bf3a3485b21bf422e0f48b858f269bea0213d93
mongkok <domake.io@gmail.com>
2017-07-09 12:10:37 +0200

    Fix task model manager

--- arcgis_marketplace/tasks.py
+++ arcgis_marketplace/tasks.py
@@ -1,12 +1,13 @@
 from celery import current_app
 
+from orders_flavor import models as orders_models
 from . import models
 
 
 @current_app.task()
 def add_item_to_account(account_id, item_id, group=None):
-    account = models.objects.get(id=account_id)
-    item = models.objects.get(id=item_id)
+    account = models.Account.objects.get(id=account_id)
+    item = orders_models.Item.objects.get(id=item_id)
 
     if group is None:
         group = account.get_group_for_apps()
